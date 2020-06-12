4b075ba928b00351da8bb03c909d25fd5ff9054c
Jocelyn Tablan <jocelyn.tablan87@gmail.com>
2020-06-12 11:09:58 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btnStart = document.querySelector(".btn__reset");
+const overlay = document.getElementById("overlay");
+let phrases = [
+  "Learning JavaScript",
+  "Front End Developer",
+  "Learning is fun",
+  "I like cheese",
+  "It is cold today",
+  "It is hot tomorrow",
+];
+let missed = [0];
+
+btnStart.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+//Return random phrase from an array
+const getRandomPhraseAsArray = (arr) => {
+  let randomPhrase = arr[Math.floor(Math.random() * phrases.length)];
+  const phraseAsArray = array.from(randomPhrase);
+  return arr[randomPhrase];
+};
+getRandomPhraseAsArray(phrases);
+
+//Adds the letters of a string to the display
+const addPhraseToDisplay = (arr) => {
+  const phraseDiv = document.getElementById("phrase");
+  for (let i = 0; i < phrases.length; i++) {
+    const currentPhrase = phrases[i];
+    const li = document.createElement("li");
+    li.innerHTML = currentPhrase;
+    phraseDiv.appendChild(li);
+  }
+};
+
+// Check if a letter is in the phrase
+// const checkLetter = button = > {};
+
+// Check if the game has been won or lost
+// const checkWin = () => {};
+
+// Listem for the start game button to be pressed
+// startButton.addEventListener('click', () => {
+
+// });
+
+// Listen for the onscreen keyboard to be clicked
+// qwerty.addEventListener('click', (e) = > {
+
+// });
