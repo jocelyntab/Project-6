acc627957e5e61a8f8c03a29c04eaa16872f9ae8
Jocelyn Tablan <jocelyn.tablan87@gmail.com>
2020-06-17 10:27:12 -0700

    changes

--- script.js
+++ script.js
@@ -10,19 +10,13 @@ let phrases = [
   "It is cold today",
   "It is hot tomorrow",
 ];
-let missed = [0];
-
-btnStart.addEventListener("click", () => {
-  overlay.style.display = "none";
-});
+let missed = 0;
 
 //Return random phrase from an array
-const getRandomPhraseAsArray = (arr) => {
-  let randomPhrase = arr[Math.floor(Math.random() * phrases.length)];
-  const phraseAsArray = Array.from(phrase);
-  return arr[randomPhrase];
-};
-getRandomPhraseAsArray(phrase);
+const getRandomPhraseAsArray = (arr) =>
+  arr[Math.floor(Math.random() * phrases.length)];
+
+const randomPhrase = getRandomPhraseAsArray(phrases);
 
 //Adds the letters of a string to the display
 const addPhraseToDisplay = (arr) => {
@@ -30,23 +24,50 @@ const addPhraseToDisplay = (arr) => {
   for (let i = 0; i < phrases.length; i++) {
     const currentPhrase = arr[i];
     const li = document.createElement("li");
+    const phraseClassName = trimAndLower(currentPhrase);
+
     li.innerHTML = currentPhrase;
+    li.classList.add("hide");
+    li.classList.add(phraseClassName);
     phraseDiv.appendChild(li);
   }
 };
 
 // Check if a letter is in the phrase
-const checkLetter = (button) => {};
+const checkLetter = (e) => {
+  const letterClicked = e.target.value;
+  let matchLetter = null;
+
+  if (randomPhrase.indexOf(letterClicked) > -1) {
+    const targetClassName = trimAndLower(randomPhrase);
+    const targetLi = document.getElementsByClassName(targetClassName)[0];
+    targetLi.classList.add("show");
+    targetLi.classList.remove("hide");
+    matchLetter = true;
+  }
+
+  return matchLetter;
+};
+
+const trimAndLower = (val) => {
+  return val.replace(" ", "").toLowerCase();
+};
 
 // Check if the game has been won or lost
 // const checkWin = () => {};
 
-// Listem for the start game button to be pressed
-// startButton.addEventListener('click', () => {
-
-// });
+// Listen for the start game button to be pressed
+btnStart.addEventListener("click", () => {
+  overlay.style.display = "none";
+  const newPhrase = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(newPhrase);
+});
 
 // Listen for the onscreen keyboard to be clicked
-// qwerty.addEventListener('click', (e) = > {
-
-// });
+qwerty.addEventListener("click", (e) => {
+  if (e.target.tagName === "button") {
+    const button = e.target;
+    button.className = "chosen";
+    button.disabled = "true";
+  }
+});
